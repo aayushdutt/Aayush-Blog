ecd7e6318bd90f12d9ffc5ed1b7b7575cd4976a7
Aayush Dutt <aayushd100@gmail.com>
2018-05-22 10:03:41 +0530

    change window to global.jQuery

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -15,11 +15,7 @@ import cleanBlog from '../assets/stylesheets/clean-blog.css'
 import paceTheme from '../assets/stylesheets/pace-theme-minimal.css'
 
 var $ = require('jquery');
-try {
-  window.jQuery = $;
-} catch (e) {
-  console.log(e)
-}
+global.jQuery = $;
 require('../assets/javascripts/bootstrap.bundle.min.js')
 require('../assets/javascripts/pace.min.js')
 require('../assets/javascripts/clean-blog.min.js')
